26848c1fbff8d14fdc3f7c8b1192f3ffff76d88c
Coin <jpatrickrutherford@gmail.com>
2017-06-21 07:18:08 -0400

    Completed everything

--- detectNetwork.js
+++ detectNetwork.js
@@ -7,12 +7,85 @@
 //   1. The first few numbers (called the prefix)
 //   2. The number of digits in the number (called the length)
 
+/*
+	Diner's Club always starts with a 38 or 39 and has a length of 14.
+	American Express always starts with a 34 or 37 and has a length of 15
+	Visa always has a prefix of 4 and a length of 13, 16, or 19.
+	MasterCard always has a prefix of 51, 52, 53, 54, or 55 and a length of 16.
+	Discover always has a prefix of 6011, 644-649, or 65, and a length of 16 or 19.
+	Maestro always has a prefix of 5018, 5020, 5038, or 6304, and a length of 12-19.
+	China UnionPay always has a prefix of 622126-622925, 624-626, or 6282-6288 and a length of 16-19.
+	Switch always has a prefix of 4903, 4905, 4911, 4936, 564182, 633110, 6333, or 6759 and a length of 16, 18, or 19.
+
+	Regular Expressions:
+	 dinersClub = /^3[89][0-9]{12}$/
+	 americanExpress = /^3[47][0-9]{13}$/
+	 visa = /^4[0-9]{12}([0-9]{3})?([0-9]{3})?$/
+	 masterCard = /^5[1-5][0-9]{14}$/
+	 discover = /^6((4[4-9])|(011)|5)([0-9]{12,17})$/
+	 maestro = /^((5018)|(5020)|(5038)|(6304))[0-9]{8,15}$/
+	 chinaUP = /^((622[1-9]\d\d)|(62[4-6])|(628[2-8]))[0-9]{10}([0-9]{1,6})?$/
+	 switch1 = /^((4903)|(4905)|(4911)|(4936)|(564182)|(633110)|(6333)|(6759))[0-9]{10}([0-9]{2,5})?$/;
+*/
+var addToArr = function(arr,start,end) {
+	for(start; start<=end; start++) {
+		arr.push(start + '');
+	}
+}
+var Card = function(name, prefix, length, regExp) {
+	this.name = name;
+	this.prefix = prefix;
+	this.length = length;
+	this.regExp = regExp;
+};
+
+var dinersClub = new Card("Diner\'s Club", ['38','39'],[14],
+						/^3[89][0-9]{12}$/);
+var americanExpress = new Card("American Express", ['34','37'], [15],
+						/^3[47][0-9]{13}$/);
+var visa = new Card("Visa", ['4'], [13,16,19],
+						/^4[0-9]{12}([0-9]{3})?([0-9]{3})?$/);
+var masterCard = new Card("MasterCard", ['51','52','53','54','55'], [16], 
+						/^5[1-5][0-9]{14}$/);
+var discover = new Card("Discover", ['6011','644','645','646','647','648','649','65'], [16,19],
+						/^6((4[4-9])|(011)|5)([0-9]{12,17})$/);
+var maestro = new Card("Maestro", ['5018','5020','5038','6304'],[12,13,14,15,16,17,18,19],
+	 					/^((5018)|(5020)|(5038)|(6304))[0-9]{8,15}$/);
+var chinaUP = new Card("China UnionPay", [], [16,17,18,19],
+					 	/^((622[1-9]\d\d)|(62[4-6])|(628[2-8]))([0-9]{10})?([0-9]{1,6})?$/);
+var switch1 = new Card("Switch",['4903','4905','4911','4936','564182','633110','6333','6759'],[16,18,19],
+						/^((4903)|(4905)|(4911)|(4936)|(564182)|(633110)|(6333)|(6759))[0-9]{10}([0-9]{2,5})?$/);
+
+addToArr(chinaUP["prefix"],622126,622925);
+addToArr(chinaUP["prefix"],624,626);
+addToArr(chinaUP["prefix"],6282,6288);
+
+var cards = [dinersClub,americanExpress,switch1,visa,masterCard,discover,maestro,chinaUP];
+
+
 var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
+
+  for(var i = 0; i < cards.length; i++) {
+  	var regularExp = cards[i].regExp;
+
+	//lazy about some of reg expression so double checking length
+	for(var j = 0; j < cards[i]["length"].length; j++) {
+
+
+		if(regularExp.test(cardNumber) && cards[i]["length"][j] === cardNumber.length) {
+			return cards[i].name;
+		}
+	}
+	
+  	
+  };
+  //if nothing passes return
+  return cardNumber;
 };
 
 
--- detectNetwork.test.js
+++ detectNetwork.test.js
@@ -8,6 +8,14 @@
 // different value.
 var FILL_ME_IN = 'Fill this value in';
  
+//function that generates random card numbers of desired length.
+var genRanNums = function (prefix, length) {
+    var str = "";
+    for(var i = 0; i < length-prefix.length; i++) {
+      str += Math.floor((Math.random() * 10));
+    }
+    return prefix + str;
+};
 describe('Introduction to Mocha Tests - READ ME FIRST', function() {
   // A Mocha test is just a function!
   // If the function throws an error when run, it fails.
@@ -18,7 +26,7 @@ describe('Introduction to Mocha Tests - READ ME FIRST', function() {
   // You will not be able to proceed with a failing test. 
 
   it('Throws an error so it fails', function() {
-    throw new Error('Delete me!');
+  //  throw new Error('Delete me!');
   });
 
   it('Doesn\'t throw an error, so it doesn\'t fail', function() {
@@ -33,7 +41,7 @@ describe('Introduction to Mocha Tests - READ ME FIRST', function() {
   // A test should only fail if the expected behavior doesn't match the actual.
   it('Throws an error when expected behavior does not match actual behavior', function() {
     var even = function(num){
-      return num/2 === 0;
+      return num%2 === 0;
     }
 
     if(even(10) !== true) {
@@ -42,10 +50,28 @@ describe('Introduction to Mocha Tests - READ ME FIRST', function() {
   });
 });
 describe('Diner\'s Club', function() {
+  var expect = chai.expect;
+  var name = dinersClub["name"];
+  var prefixs = dinersClub["prefix"];
+  var arrLengths = dinersClub["length"];
+  //Generate Tests
+  for(var i = 0; i < arrLengths.length; i++) {
+    for (var j = 0; j < prefixs.length; j++) {
+      (function(prefix) {
+        //creates a random number with desired prefix and length.
+        
+        var cardNum = genRanNums(prefix,arrLengths[i]);
+        it('has a prefix of ' + prefix + ' and a length of ' + arrLengths[i], function() {
+        expect(detectNetwork(cardNum)).to.equal(name);
+        });
+      })(prefixs[j])
+    };
+  };
+
   // Be careful, tests can have bugs too...
 
   it('has a prefix of 38 and a length of 14', function() {
-    throw new Error('Delete me!');
+  //  throw new Error('Delete me!');
  
     if (detectNetwork('38345678901234') !== 'Diner\'s Club') {
       throw new Error('Test failed');
@@ -53,7 +79,7 @@ describe('Diner\'s Club', function() {
   });
 
   it('has a prefix of 39 and a length of 14', function() {
-    if (detectNetwork('3934567890123') !== 'Diner\'s Club') {
+    if (detectNetwork('39345678901234') !== 'Diner\'s Club') {
       throw new Error('Test failed');
     }
  
@@ -63,6 +89,24 @@ describe('Diner\'s Club', function() {
 describe('American Express', function() {
   // It can get annoying to keep typing the if/throw, so here is a
   // helper function to throw an error if the input statement isn't true. 
+  var expect = chai.expect;
+  var name = americanExpress["name"];
+  var prefixs = americanExpress["prefix"];
+  var arrLengths = americanExpress["length"];
+  //Generate Tests
+  for(var i = 0; i < arrLengths.length; i++) {
+    for (var j = 0; j < prefixs.length; j++) {
+      (function(prefix) {
+        //creates a random number with desired prefix and length.
+        
+        var cardNum = genRanNums(prefix,arrLengths[i]);
+        it('has a prefix of ' + prefix + ' and a length of ' + arrLengths[i], function() {
+        expect(detectNetwork(cardNum)).to.equal(name);
+        });
+      })(prefixs[j])
+    };
+  };
+
   var assert = function(isTrue) {
     if(isTrue) {
       throw new Error('Test failed');
@@ -71,11 +115,11 @@ describe('American Express', function() {
   };
 
   it('has a prefix of 34 and a length of 15', function() {
-    assert(detectNetwork('343456789012345') === 'American Express');
+    assert(detectNetwork('343456789012345') !== 'American Express');
   });
 
   it('has a prefix of 37 and a length of 15', function() {
-    assert(detectNetwork('373456789012345') === 'American Express');
+    assert(detectNetwork('373456789012345') !== 'American Express');
   });
 });
 
@@ -84,7 +128,27 @@ describe('Visa', function() {
   // Chai provides an assert that acts the same as our previous assert.
   // Search the documentation to figure out how to access it. 
   //   http://chaijs.com/
-  var assert = chai.FILL_ME_IN;
+
+  var expect = chai.expect;
+  var name = visa["name"];
+  var prefixs = visa["prefix"];
+  var arrLengths = visa["length"];
+  //Generate Tests
+  for(var i = 0; i < arrLengths.length; i++) {
+    for (var j = 0; j < prefixs.length; j++) {
+      (function(prefix) {
+        //creates a random number with desired prefix and length.
+        
+        var cardNum = genRanNums(prefix,arrLengths[i]);
+        it('has a prefix of ' + prefix + ' and a length of ' + arrLengths[i], function() {
+        expect(detectNetwork(cardNum)).to.equal(name);
+        });
+      })(prefixs[j])
+    };
+  };
+
+  var assert = chai.assert;
+
  
 
   it('has a prefix of 4 and a length of 13', function() {
@@ -106,16 +170,32 @@ describe('MasterCard', function() {
   // If you want to know more, check out the documentation. 
   //   http://chaijs.com/api/bdd/
   var expect = chai.expect;
+  var name = masterCard["name"];
+  var prefixs = masterCard["prefix"];
+  var arrLengths = masterCard["length"];
+  //Generate Tests
+  for(var i = 0; i < arrLengths.length; i++) {
+    for (var j = 0; j < prefixs.length; j++) {
+      (function(prefix) {
+        //creates a random number with desired prefix and length.
+        
+        var cardNum = genRanNums(prefix,arrLengths[i]);
+        it('has a prefix of ' + prefix + ' and a length of ' + arrLengths[i], function() {
+        expect(detectNetwork(cardNum)).to.equal(name);
+        });
+      })(prefixs[j])
+    };
+  };
  
-  it(FILL_ME_IN, function() {
+  it('has a prefix of 51 and a length of 15', function() {
     expect(detectNetwork('5112345678901234')).to.equal('MasterCard');
   });
  
-  it(FILL_ME_IN, function() {
+  it('has a prefix of 52 and a length of 15', function() {
     expect(detectNetwork('5212345678901234')).to.equal('MasterCard');
   });
  
-  it(FILL_ME_IN, function() {
+  it('has a prefix of 53 and a length of 15', function() {
     expect(detectNetwork('5312345678901234')).to.equal('MasterCard');
   });
  
@@ -127,28 +207,106 @@ describe('MasterCard', function() {
   // and should, but that's just for learning), so once you've gotten 
   // these tests to pass using should syntax, refactor your tests to 
   // use either expect or should, but not both. 
-  var should = chai.should();
+  // var should = chai.should();
   
   it('has a prefix of 54 and a length of 16', function() {
-    detectNetwork('5412345678901234').should.equal(FILL_ME_IN);
+    expect(detectNetwork('5412345678901234')).to.equal('MasterCard');
   });
  
   it('has a prefix of 55 and a length of 16', function() {
-    detectNetwork('5512345678901234').should.equal(FILL_ME_IN);
-  })
+    expect(detectNetwork('5512345678901234')).to.equal('MasterCard');
+  });
  
 });
 
 describe('Discover', function() {
   // Tests without a function will be marked as "pending" and not run
   // Implement these tests (and others) and make them pass!
-  it('has a prefix of 6011 and a length of 16');
-  it('has a prefix of 6011 and a length of 19');
+  var expect = chai.expect;
+  var name = discover["name"];
+  var prefixs = discover["prefix"];
+  var arrLengths = discover["length"];
+  //Generate Tests
+  for(var i = 0; i < arrLengths.length; i++) {
+    for (var j = 0; j < prefixs.length; j++) {
+      (function(prefix) {
+        //creates a random number with desired prefix and length.
+        
+        var cardNum = genRanNums(prefix,arrLengths[i]);
+        it('has a prefix of ' + prefix + ' and a length of ' + arrLengths[i], function() {
+        expect(detectNetwork(cardNum)).to.equal(name);
+        });
+      })(prefixs[j])
+    };
+  };
+  
+
+
+
+  it('has a prefix of 6011 and a length of 16', function() {
+    expect(detectNetwork('6011123456789012')).to.equal('Discover');
+  });
+  it('has a prefix of 6011 and a length of 19', function() {
+    expect(detectNetwork('6011123456789012345')).to.equal('Discover');
+  });
 });
 
 describe('Maestro', function() {
   // Write full test coverage for the Maestro card
+  var expect = chai.expect;
+  var name = maestro["name"];
+  var prefixs = maestro["prefix"];
+  var arrLengths = maestro["length"];
+  //Generate Tests
+  for(var i = 0; i < arrLengths.length; i++) {
+    for (var j = 0; j < prefixs.length; j++) {
+      (function(prefix) {
+        //creates a random number with desired prefix and length.
+        
+        var cardNum = genRanNums(prefix,arrLengths[i]);
+        it('has a prefix of ' + prefix + ' and a length of ' + arrLengths[i], function() {
+        expect(detectNetwork(cardNum)).to.equal(name);
+        });
+      })(prefixs[j])
+    };
+  };
+
 });
 
-describe('should support China UnionPay')
-describe('should support Switch')
+describe('should support China UnionPay', function() {
+  var expect = chai.expect;
+  var name = chinaUP["name"];
+  var prefixs = chinaUP["prefix"];
+  var arrLengths = chinaUP["length"];
+  //Generate Tests
+  for(var i = 0; i < arrLengths.length; i++) {
+    for (var j = 0; j < prefixs.length; j++) {
+      (function(prefix) {
+        //creates a random number with desired prefix and length.
+        
+        var cardNum = genRanNums(prefix,arrLengths[i]);
+        it('has a prefix of ' + prefix + ' and a length of ' + arrLengths[i], function() {
+        expect(detectNetwork(cardNum)).to.equal(name);
+        });
+      })(prefixs[j])
+    };
+  };
+});
+describe('should support Switch', function() {
+  var expect = chai.expect;
+  var name = switch1["name"];
+  var prefixs = switch1["prefix"];
+  var arrLengths = switch1["length"];
+  //Generate Tests
+  for(var i = 0; i < arrLengths.length; i++) {
+    for (var j = 0; j < prefixs.length; j++) {
+      (function(prefix) {
+        //creates a random number with desired prefix and length.
+        var cardNum = genRanNums(prefix,arrLengths[i]);
+        it('has a prefix of ' + prefix + ' and a length of ' + arrLengths[i], function() {
+        expect(detectNetwork(cardNum)).to.equal(name);
+        });
+      })(prefixs[j])
+    };
+  };
+})
